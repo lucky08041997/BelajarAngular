b82787f2ef1fa94375fe408be6d5262a39420257
lucky08041997 <luckycahyahandika@gmail.com>
2020-10-16 20:56:16 +0700

    Definisi Group Form

--- WebCRUD/src/app/app.module.ts
+++ WebCRUD/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { EmployeesComponent } from './employees/employees.component';
 import { EmployeeComponent } from './employees/employee/employee.component';
+import { EmployeeService } from './employee.service';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,7 @@ import { EmployeeComponent } from './employees/employee/employee.component';
     AppRoutingModule,
     BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [EmployeeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
