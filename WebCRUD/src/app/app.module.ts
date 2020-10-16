ff4a53ea25391478e1c0c2ba3209d5cedc0b6603
lucky08041997 <luckycahyahandika@gmail.com>
2020-10-16 20:46:46 +0700

    Pembuatan proyek baru dan instal Angular Material

--- WebCRUD/src/app/app.module.ts
+++ WebCRUD/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EmployeesComponent } from './employees/employees.component';
+import { EmployeeComponent } from './employees/employee/employee.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeesComponent,
+    EmployeeComponent
   ],
   imports: [
     BrowserModule,
