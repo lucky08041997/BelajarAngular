db5d9210c3e8ec060af531f087937cb284db38e8
lucky08041997 <luckycahyahandika@gmail.com>
2020-10-18 19:23:16 +0700

    Finally Done

--- WebCRUD/src/app/app.module.ts
+++ WebCRUD/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,6 +20,7 @@ import { MaterialModule } from './material/material.module';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    ReactiveFormsModule,
     MaterialModule
   ],
   providers: [EmployeeService],
